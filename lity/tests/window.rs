8dc4ecb1f60d8157c765b3a4f7f4c72dda0a0b3e
Werick da Silva <werick.s.santana34@gmail.com>
2022-12-02 12:04:44 -0300

    :test_tube: test: window configuration tests

---
+++ lity/tests/window.rs
@@ -0,0 +1,42 @@
+use lity::{application, Point, Size, Window};
+
+#[test]
+pub fn show_window() {
+    application(Window::new())
+}
+
+#[test]
+pub fn title_window() {
+    let window = Window::new().set_title(String::from("Title Window"));
+    application(window)
+}
+
+#[test]
+fn size_window() {
+    use lity::{application, kurbo::Size, Window};
+
+    let window = Window::new().set_size(Size::new(700., 300.));
+
+    application(window)
+}
+#[test]
+fn position_window() {
+    let window = Window::new().set_positio(Point::new(400., 300.));
+
+    application(window)
+}
+#[test]
+fn resizable_window() {
+    let window = Window::new().set_resizable(true);
+    application(window)
+}
+#[test]
+fn show_config_window() {
+    let window = Window::new()
+        .set_title("Show Config Window".to_string())
+        .set_size(Size::new(200., 200.))
+        .set_resizable(false)
+        .set_positio(Point::new(0., 0.));
+    
+        application(window)
+}
